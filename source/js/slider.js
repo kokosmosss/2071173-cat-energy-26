9cbed974df2fc2d87900efd2910c1994c5c2dfee
Мария Гаврилова <topik2344@gmail.com>
2022-08-28 23:47:49 +0300

    Добавляет адаптацию графика, слайдер

---
+++ source/js/slider.js
@@ -0,0 +1,79 @@
+const slider = document.querySelector('.slider');
+const before = document.querySelector('.slider__image-wrapper--before');
+const beforeImage = before.querySelector('img');
+const after = document.querySelector('.slider__image-wrapper--after');
+const afterImage = after.querySelector('img');
+const toggle = document.querySelector('.slider__bar');
+const body = document.body;
+
+let isActive = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = `${width}px`;
+  afterImage.style.width = `${width}px`;
+});
+
+const beforeAfterSlider = (x) => {
+  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = `${shift}px`;
+  toggle.style.left = `${shift}px`;
+};
+
+const pauseEvents = (e) => {
+  e.stopPropagation();
+  e.preventDegault();
+  return false;
+}
+
+body.addEventListener('mousedown', () => {
+  isActive = true;
+});
+
+body.addEventListener('mouseup', () => {
+  isActive = false;
+});
+
+body.addEventListener('mouseleave', () => {
+  isActive = false;
+});
+
+body.addEventListener('mousemove', (e) => {
+  if (!isActive) {
+    return;
+  }
+
+  let x = e.pageX;
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
+
+// body.addEventListener('touchstart', () => {
+//   isActive = true;
+// });
+
+// body.addEventListener('touchend', () => {
+//   isActive = false;
+// });
+
+// body.addEventListener('touchcancel', () => {
+//   isActive = false;
+// });
+
+// body.addEventListener('touchmove', (e) => {
+//   if (!isActive) {
+//     return;
+//   }
+
+//   let x;
+//   let i;
+
+//   for (i = 0; e < e.changedTouches; i++) {
+//     x = e.changedTouches[i].pageX;
+//   }
+
+//   x -= slider.getBoundingClientRect().left;
+//   beforeAfterSlider(x);
+//   pauseEvents(e);
+// });
