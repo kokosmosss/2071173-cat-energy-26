892783fe6501e14f16d3b7a871acf430eb1380e6
Мария Гаврилова <topik2344@gmail.com>
2022-09-18 23:59:06 +0300

    Исправляет по замечаниям

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,9 +2,6 @@ let header = document.querySelector('.page-header');
 let headerToggle = document.querySelector('.page-header__toggle');
 
 header.classList.remove('page-header--no-js');
-if (window.innerWidth < 768) {
-  header.classList.add('page-header--closed');
-}
 
 headerToggle.addEventListener('click', function () {
   if (header.classList.contains('page-header--closed')) {
