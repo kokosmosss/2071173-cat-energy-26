ad7edf9fcea1688b0b95d1b4ceb376bd2ab8109b
Мария Гаврилова <topik2344@gmail.com>
2022-09-20 12:26:06 +0300

    исправляет по замечаниям к второй проверке

---
+++ source/js/map.js
@@ -0,0 +1,5 @@
+let contactsMapImage = document.querySelector('.contacts__map-image');
+let contactsMap = document.querySelector('.contacts__map');
+
+contactsMapImage.classList.remove('contacts__map-image--nojs');
+contactsMap.classList.remove('contacts__map--nojs');
